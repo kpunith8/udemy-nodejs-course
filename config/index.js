1fc0505a30ed78dfd27c829c3d66e7a186adeef8
Punith K <kpunith8@gmail.com>
2020-11-07 20:54:32 +0530

    Added mongo session storage and cookies

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : 3002;
+const DB_URI =
+  'mongodb+srv://root:root@mongo-cluster-qclzr.mongodb.net/shopping?retryWrites=true&w=majority';
 
-module.exports = { PORT };
+module.exports = { PORT, DB_URI };
