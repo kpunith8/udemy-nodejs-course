e69629f77a3a25fda43ad3b334933d33e4e902ef
Punith K <kpunith8@gmail.com>
2022-11-17 13:41:12 +0530

    Update mongo URI to the latest one

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : 3002;
 const DB_URI =
-  'mongodb+srv://root:root@mongo-cluster-qclzr.mongodb.net/shopping?retryWrites=true&w=majority';
+  'mongodb+srv://root:root@node-shop.qnaixik.mongodb.net/?retryWrites=true&w=majority';
 
 module.exports = { PORT, DB_URI };
